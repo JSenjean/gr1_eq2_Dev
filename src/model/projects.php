d642e6c1225e47524814ea9b08b6992b78ab76c1
Math64100 <math64100@hotmail.fr>
2019-10-30 15:42:04 +0100

    correction requete SQL

--- src/model/projects.php
+++ src/model/projects.php
@@ -8,7 +8,7 @@ function get_all_project_by_id($id)
     try {
         $bdd = dbConnect();
         $stmt = $bdd->prepare(
-            "SELECT P.id, P.name, P.description, P.visibility, pm.role
+            "SELECT p.id, p.name, p.description, p.visibility, pm.role
                     FROM project=p,project_member=pm
                     WHERE p.id=pm.project_id AND pm.user_id=:usrId"
         );
