4a85ba42bb7ddd97aa9ccc1d0c997c6df993bf0e
gfloret <gu.floret@hotmail.com>
2019-10-31 16:06:40 +0100

    Fix

--- src/model/index.php
+++ src/model/index.php
@@ -8,7 +8,6 @@
     function dbConnect(){
       $config = parse_ini_file('config.ini');
       $bdd = new PDO("mysql:host=".$config['servername'].";dbname=".$config['dbname'].";charset=utf8", $config['username'], $config['password'], array ( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
-      //$bdd = new PDO("mysql:host=localhost;dbname=projetweb2", 'root', '', array ( PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
       return $bdd;
     }
 
