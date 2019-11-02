539630924d4d1b5e097889e655303f6488c3f9e0
gfloret <gu.floret@hotmail.com>
2019-11-02 15:25:10 +0100

    View mise à jour projects.php
    View mise à jour projects.php

--- src/view/projects.php
+++ src/view/projects.php
@@ -1,111 +1,126 @@
-    <div class="row">
-        <!-- mes projets -->
-        <div class="col-lg-6 card my-custom-scrollbar bg-light">
-            <div class="card-header row">
-                <div class="col-md-8">
-                    <h2>Mes projets </h2>
-                </div>
-                <div class="col-md-4 float-right">
-                    <a data-target='#NewProjectModal' href='index.php?action=newProject' data-toggle="modal" class="confirmNewProjectModalLink">
-                        <button class="float-sm-right btn-block btn btn-primary" type="button" >
-                            Nouveau projet
-                            <i class='fas fa-plus-square' style="color:white ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Ajouter membre"></i>
-                        </button>
-                    </a>
-                </div>
-            </div>
-            <?php
-            foreach ($projects as $u) {
-                //print_r($u);
-                ?>
-                <div class="card mt-3">
-                    <div class="card-header row">
-                        <h3>
-                            <?php echo ($u['name']); //project name
-                                ?>
-                            <?php if ($u['role'] == 'master') { ?>
-                                <i class='fas fa-crown' style="color:yellow"></i>
-                            <?php
-                                }
-                                ?>
-                    </div>
+<div class="row pt-5">
+    
+    <!-- Create and manage projects -->
+    <div class="col-sm">
+    <div class="card">
+    <div class="card-body">
 
-                    </h3>
-                    <div class="card-footer " style="background-color: #E0DAD9">
-                        <?php
-                            if ($u['role'] == 'master') { ?>
-                            <a data-target='#deleteProjectModal' href='index.php?action=projectDelete&projectId=<?php echo ($u['id']) ?>' data-toggle="modal" class="confirmDeleteProjectModalLink">
-                                <i class='fas fa-trash' style="color:red; cursor:pointer" data-toggle="tooltip" data-placement="top" title="supprimer projet"></i>
-                            </a>
-
-                            <a href='index.php?action=addMemberToProject&projectId=<?php echo ($u['id']) ?>'>
-                                <i class='fas fa-plus-square' style="color:blue ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Ajouter membre"></i>
-                            </a>
-
-                        <?php
-                            } else {
-                                ?>
-                            <a href='index.php?action=leaveProject&projectId=<?php echo ($u['id']) ?>'>
-                                <i class='fas fa-arrow-alt-circle-left	 ' style="color:red ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Entrer dans le projet"></i>
-                            </a>
-                        <?php } ?>
-                        <a href='index.php?action=selectedProject&projectId=<?php echo ($u['id']) ?>'>
-                            <i class='fas fa-arrow-alt-circle-right	 ' style="color:green ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Entrer dans le projet"></i>
-                        </a>
-                    </div>
-                </div>
-            <?php
-
-            }
-            ?>
+        <div class="clearfix">
+            <h1 class="text-dark card-link">Mes Projets</h1>
+            <a role="button" class="btn btn-primary mb-4 mr-2 confirmNewProjectModalLink" data-target='#NewProjectModal' href='index.php?action=newProject' data-toggle="modal">
+                Créer un projet
+            </a>
         </div>
 
+        <div class="list-group">
+            <?php foreach ($projects as $u) {?>
 
-        <!-- search a project -->
-        <div class="col-lg-6 card my-custom-scrollbar bg-light">
+            <div class="list-group-item flex-column align-items-start">
+                <div class="media">
 
-            <div class="card-header row">
-                <div class="col-md-8 active-cyan-3 active-cyan-4">
-                    <input class="form-control" type="text" placeholder="Rechercher Projet" aria-label="Search" id="myInputSearch">
-                </div>
-            </div>
-            <div id="searchProjectDiv">
-                <?php
-                foreach ($otherProjects as $u) {
-                    //print_r($otherProjects);
-                    if ($u['visibility'] == 1) {
-                        ?>
-                        <div class="card mt-3">
-                            <div class="card-header row">
-                                <div class="col-lg-6">
-                                    <h3>
-                                        <?php echo ("projet : " . $u['name']."     "); //project name
-                                                ?>
-                                    </h3>
-                                </div>
-                                <div class="col-lg-6">
-                                    <h4 class="align-right"><?php echo (" Chef du projet : " . $u['username']); ?></h4>
-                                </div>
-                            </div>
-
-
-                            <div class="card-footer " style="background-color: #E0DAD9">
-                                <button class="float-sm-right btn-block btn btn-primary" type="button" data-target="#deleteProjectModal">
-                                    Demander a rejoindre
-                                    <i class='fas fa-plus-square' style="color:white ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Ajouter membre"></i>
-                                </button>
-                            </div>
+                    <div class="media-body">
+                        <div class="d-flex justify-content-between">
+                            <h5 class="mt-0"><?php echo $u['name']?></h5>
+                            <h6 class="text-muted">
+                                <span class="font-weight-bold">
+                                    <?php if ($u['role'] == 'master') { ?>
+                                        <i class='fas fa-crown' style="color:#F3E90A"></i>
+                                    <?php } ?>
+                                </span>
+                            </h6>
                         </div>
-                <?php
 
-                    }
-                }
-                ?>
+                        <div class="d-flex justify-content-between">
+                            <h6 class="text-muted">
+                                <span class="font-weight-bold">
+                                    <?php if ($u['role'] == 'master') { ?>
+                                        <a href='index.php?action=addMemberToProject&projectId=<?php echo ($u['id']) ?>'>
+                                            <i class='fas fa-plus-square' style="color:blue ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Ajouter des membres"></i>
+                                        </a>
+                                        <a data-target='#deleteProjectModal' href='index.php?action=projectDelete&projectId=<?php echo ($u['id']) ?>' data-toggle="modal" class="confirmDeleteProjectModalLink">
+                                            <i class='fas fa-trash' style="color:red; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Supprimer projet"></i>
+                                        </a>
+                                    <?php } else { ?>
+                                        <a href='index.php?action=leaveProject&projectId=<?php echo ($u['id']) ?>'>
+                                            <i class='fas fa-arrow-alt-circle-left	 ' style="color:red ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Quitter le projet"></i>
+                                        </a>
+                                    <?php } ?>
+                                </span>
+                            </h6>
+                            <h6>
+                                <span class="font-weight-bold">
+                                    <a href='index.php?action=selectedProject&projectId=<?php echo ($u['id']) ?>'>
+                                        <i class='fas fa-arrow-alt-circle-right	 ' style="color:green ; cursor:pointer" data-toggle="tooltip" data-placement="top" title="Accéder au projet"></i>
+                                    </a>
+                                </span>
+                            </h6>
+                        </div>
+                    </div>
+
+                </div>
             </div>
+
+            <?php } ?>
+
         </div>
+
+    </div>
+    </div>
     </div>
 
+    <br>
 
+    <!-- Search a project -->
+    <div class="col-sm">
+    <div class="card">
+    <div class="card-body">
+
+    <div class="clearfix">
+
+        <h1 class="text-dark card-link">Rechercher un projet</h1>
+        <input class="form-control" type="text" placeholder="Rechercher Projet" aria-label="Search" id="myInputSearch">
+        <br>
+        
+        <div class="list-group">
+            <?php foreach ($otherProjects as $u) {
+                if ($u['visibility'] == 1) { ?>
+                    <div class="list-group-item flex-column align-items-start">
+                        <div class="media">
+
+                            <div class="media-body">
+                                <div class="d-flex justify-content-between">
+                                    <h5 class="mt-0"><?php echo $u['name']?></h5>
+                                    <h6 class="test-muted">
+                                        <span class="font-weight-bold">Chef du projet : </span>
+                                        <?php echo ($u['username']); ?>
+                                    </h6>
+                                </div>
+
+                                <div class="d-flex justify-content-between">
+                                    <h6 class="text-muted">
+                                        <span class="font-weight-bold">
+                                            <a role="button" class="btn btn-success mb-4 mr-2" data-target='#JoinProject' href='' data-toggle="modal">
+                                                Demander à rejoindre
+                                            </a>
+                                        </span>
+                                    </h6>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+            <?php } } ?>
+        </div>
+
+
+
+
+
+    </div>
+
+    </div>
+    </div>
+    </div>
 
     </body>
 
