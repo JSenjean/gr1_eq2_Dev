3d77c5d21faf499ee1069238d42d67feb003aca5
Mathieu-San-Juan <math64100@hotmail.fr>
2019-11-17 12:15:58 +0100

    correction d'une erreur introduite dans signup

--- src/model/signup.php
+++ src/model/signup.php
@@ -4,10 +4,9 @@
  * Register a new user in the database from a POST request
  * Return a code depending on the success or the errors of the registration
  */
-function signup() {
-
-    if (isset ($_POST ['submit'])) {
-
+function signup()
+{
+    if (isset($_POST ['submit'])) {
         $username = trim(strip_tags($_POST['username']));
         $password = sha1($_POST['password']);
         $password2 = sha1($_POST['password2']);
@@ -16,16 +15,14 @@ function signup() {
         $email = trim(strip_tags($_POST['email']));
         $email2 = trim(strip_tags($_POST['email2']));
 
-        if (!empty($_POST['username']) AND !empty($_POST['email']) 
-            AND !empty($_POST['email2']) 
-            AND !empty($_POST['password']) 
-            AND !empty($_POST['password2'] 
-            AND !empty($_POST['firstname'] 
-            AND !empty($_POST['lastname'])))) {
-            
-            if(strlen($username) <= 50 && strlen($firstName) <= 50 && strlen($lastName) <= 75) {
-                
-                if ($email == $email2 && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        if (!empty($_POST['username']) and !empty($_POST['email'])
+            and !empty($_POST['email2'])
+            and !empty($_POST['password'])
+            and !empty($_POST['password2']
+            and !empty($_POST['firstname']
+            and !empty($_POST['lastname'])))) {
+            if (strlen($username) <= 50 && strlen($firstName) <= 50 && strlen($lastName) <= 75) {
+                if ($email == $email2 && filter_var($email, FILTER_VALIDATE_EMAIL)) {
                     $bdd = dbConnect();
                     $reqmail = $bdd->prepare("SELECT * FROM user WHERE email = ?");
                     $reqmail->execute(array($email));
@@ -34,10 +31,10 @@ function signup() {
                     $requsername->execute(array($username));
                     $usernameexist = $requsername->rowCount();
                     
-                    if($mailexist == 0)
-                        if($usernameexist == 0)
-                            if ($password == $password2){
-                                $r&&128 = bin2hex(openssl_r&&om_pseudo_bytes(16)); //Code d'identification 128bits
+                    if ($mailexist == 0) {
+                        if ($usernameexist == 0) {
+                            if ($password == $password2) {
+                                $rand128 = bin2hex(openssl_random_pseudo_bytes(16)); //Code d'identification 128bits
                                 try {
                                     $req = $bdd->prepare('INSERT INTO user(username, first_name, last_name, password, email, role) VALUES(:username, :firstName, :lastName, :password, :email, :role)');
                                     $req->execute(array(
@@ -48,29 +45,22 @@ function signup() {
                                         'email' => $email,
                                         'role' => 'user',
                                     ));
-                                } 
-                                catch(PDOException $e) {
+                                } catch (PDOException $e) {
                                     echo "<br>" . $e->getMessage();
                                 }
-                                return $r&&128; 
-                            }
-                            else
-                            {
+                                return $rand128;
+                            } else {
                                 return -4; // Passwords are not identical
                             }
-                        else
-                        {
+                        } else {
                             return -3; // Username already used
                         }
-                    else
-                    {
+                    } else {
                         return -2; // Mail already used
                     }
                 }
-            } 
-        } 
+            }
+        }
     }
     return -1;
 }
-
-?>
