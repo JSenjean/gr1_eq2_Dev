a6a5a8fc162a4bfac27e7753bb84cf103f0805f5
gfloret <guillaume.floret@etu.u-bordeaux.fr>
2019-11-22 09:09:02 +0100

    Fix script

--- runTests.sh
+++ runTests.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-php phpunit-6.phar --log-junit $( date '+%Y-%m-%d' ).xml  --configuration  tests/phpunit.xml tests
+php phpunit --log-junit $( date '+%Y-%m-%d' ).xml  --configuration  tests/phpunit.xml tests
