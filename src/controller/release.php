4dd407340dfffafb2c6e88c6bf9929c10ec0db78
Mathieu San Juan <msjuan@emi.u-bordeaux1.fr>
2019-11-25 13:29:43 +0100

    T4 : US22/US23 début de la vue de release qui acceuillera les différents element recuperer a partie de l'api de git

--- src/controller/release.php
+++ src/controller/release.php
@@ -1,6 +1,9 @@
 <?php
 
 require_once "model/selectedProject.php";
+require_once "model/release.php";
+
+$UserID = $_SESSION["id"];
 
 $projectId = $_GET["projectId"];
 $isMaster = is_master($UserID, $projectId);
@@ -14,6 +17,17 @@ if ($_SESSION['role'] == 'user') {
 } else {
      include_once "view/modHeader.php" ;
 }
+
+
+     $c = curl_init();
+     curl_setopt($c, CURLOPT_RETURNTRANSFER, 1);
+     curl_setopt($c, CURLOPT_HTTPHEADER, array('Accept: application/json', 'Content-Type: application/json'));
+     curl_setopt($c, CURLOPT_URL, 'https://api.github.com/users/google/repos');
+     $content = curl_exec($c);
+     curl_close($c);
+     $api = json_decode($content);
+     print($api->open_issues_count);
+
 
 include_once "view/projectNav.php";
 include_once "view/release.php";
