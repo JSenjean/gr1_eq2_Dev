917eb762f21869d4daab43052d15540ae0e8a05f
JSenjean <senjean.joel@gmail.com>
2019-12-03 12:31:32 +0100

    T9
    Fix

--- tests/dbConnectTest.php
+++ tests/dbConnectTest.php
@@ -3,7 +3,7 @@
 function dbConnect()
 {
     $config = parse_ini_file('configTest.ini');
-    return $bdd = new PDO("mysql:host=" . $config['servername'] . ";dbname=" . $config['dbname'] . ";charset=utf8", $config['username'], $config['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    return new PDO("mysql:host=" . $config['servername'] . ";dbname=" . $config['dbname'] . ";charset=utf8", $config['username'], $config['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 
 function clear_Database()
